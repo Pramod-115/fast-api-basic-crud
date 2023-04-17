b54793906faef8313219dcd44b6fe004d2a5a4a0
Pramod <pramodthapa115@gmail.com>
2023-04-17 18:03:07 +0545

    feat: add routes

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Blog(BaseModel):
+    name: str
+    content: str
 
 app = FastAPI()
 
-@app.get('/')
+@app.put('/blogs')
+def create_blog(blog: Blog):
+    return {"message": 'Blog created', "data": blog}
+
+@app.get('/blogs')
 def get_Blogs():
     return {"blogs": 'Lists of Blogs'}
