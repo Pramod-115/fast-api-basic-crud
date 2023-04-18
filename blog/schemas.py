fe8d9e4c93beda4f51f27a9356e20845192ca273
Pramod <pramodthapa115@gmail.com>
2023-04-18 18:09:21 +0545

    feat: add user schema

--- blog/schemas.py
+++ blog/schemas.py
@@ -1,5 +1,26 @@
 from pydantic import BaseModel
 
 class Blog(BaseModel):
+    title: str
+    body: str
+
+class UpdateBlog(Blog):
+    class Config():
+        partial = True
+
+# UpdateBlog = Blog.partial_model()
+
+class ShowBlog(BaseModel):
+    title: str
+    class Config():
+        orm_mode = True
+
+
+class User(BaseModel):
     name: str
-    content: str
+    email: str
+    password: str
+
+class ShowUser(BaseModel):
+    name: str
+    email: str
